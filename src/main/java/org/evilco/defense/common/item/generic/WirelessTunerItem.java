667c657ba8265e03e7a139622e1c5c4afcca0a75
Johannes Donath <johannesd@evil-co.com>
2014-05-16 04:35:29 +0200

    Fixed item use being processed twice.

--- src/main/java/org/evilco/defense/common/item/generic/WirelessTunerItem.java
+++ src/main/java/org/evilco/defense/common/item/generic/WirelessTunerItem.java
@@ -15,6 +15,8 @@
  */
 package org.evilco.defense.common.item.generic;
 
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.relauncher.Side;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -55,6 +57,9 @@ public class WirelessTunerItem extends Item {
 	 */
 	@Override
 	public boolean onItemUse (ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
+		// skip client version
+		if (FMLCommonHandler.instance ().getEffectiveSide () == Side.CLIENT) return false;
+
 		// check whether players may edit the block
 		if (!par2EntityPlayer.canPlayerEdit (par4, par5, par6, par7, par1ItemStack)) return false;
 
