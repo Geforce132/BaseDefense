64321d82381585aeb99047cee7b8a65954f04bbb
Johannes Donath <johannesd@evil-co.com>
2014-05-17 23:21:11 +0200

    Added missing JavaDoc.

--- src/main/java/org/evilco/defense/common/item/surveillance/SecurityBotItem.java
+++ src/main/java/org/evilco/defense/common/item/surveillance/SecurityBotItem.java
@@ -45,6 +45,9 @@ public class SecurityBotItem extends Item {
 		this.setUnlocalizedName (Strings.ITEM_SURVEILLANCE_SECURITY_BOT);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean onItemUse (ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
 		// skip client version
