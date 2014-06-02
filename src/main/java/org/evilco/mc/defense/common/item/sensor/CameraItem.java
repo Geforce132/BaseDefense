336aac1a8a4b11776eb2dc43e8e774f1435a96da
Johannes Donath <johannesd@evil-co.com>
2014-06-02 09:51:19 +0200

    Fixed localization.

--- src/main/java/org/evilco/mc/defense/common/item/sensor/CameraItem.java
+++ src/main/java/org/evilco/mc/defense/common/item/sensor/CameraItem.java
@@ -68,6 +68,14 @@ public class CameraItem extends Item {
 	 * {@inheritDoc}
 	 */
 	@Override
+	public String getUnlocalizedName (ItemStack par1ItemStack) {
+		return super.getUnlocalizedName (par1ItemStack) + par1ItemStack.getItemDamage ();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
 	public boolean onItemUse (ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
 		// skip client execution
 		if (par3World.isRemote) return true;
