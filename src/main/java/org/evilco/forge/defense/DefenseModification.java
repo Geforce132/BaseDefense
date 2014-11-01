d07a27014ccf19d0e13f6b7f26abecae3a1bdb9d
Johannes Donath <johannesd@evil-co.com>
2014-11-01 15:57:57 +0100

    Added event handlers and support for logging.

--- src/main/java/org/evilco/forge/defense/DefenseModification.java
+++ src/main/java/org/evilco/forge/defense/DefenseModification.java
@@ -15,7 +15,13 @@
 package org.evilco.forge.defense;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import lombok.Getter;
+import org.apache.logging.log4j.Logger;
+import org.evilco.forge.defense.common.CommonModificationProxy;
 
 /**
  * @author Johannes Donath <johannesd@evil-co.com>
@@ -35,4 +41,50 @@ public class DefenseModification {
 	@Mod.Instance (MOD_ID)
 	@Getter
 	private static DefenseModification instance = null;
+
+	/**
+	 * Stores the active proxy instance.
+	 */
+	@SidedProxy (serverSide = "org.evilco.forge.defense.common.CommonModificationProxy", clientSide = "org.evilco.forge.defense.client.ClientModificationProxy")
+	private IModificationProxy proxy = null;
+
+	/**
+	 * Stores the logger.
+	 */
+	@Getter
+	private Logger logger = null;
+
+	/**
+	 * Callback for {@link cpw.mods.fml.common.event.FMLPreInitializationEvent}.
+	 * @param event The event.
+	 */
+	@Mod.EventHandler
+	public void onPreInitialize (FMLPreInitializationEvent event) {
+		// store logger
+		this.logger = event.getModLog ();
+
+		// call proxy
+		this.proxy.preInitialize ();
+	}
+
+	/**
+	 * Callback for {@link cpw.mods.fml.common.event.FMLInitializationEvent}.
+	 * @param event The event.
+	 */
+	@Mod.EventHandler
+	public void onInitialize (FMLInitializationEvent event) {
+		this.proxy.initialize ();
+	}
+
+	/**
+	 * Callback for {@link cpw.mods.fml.common.event.FMLPostInitializationEvent}.
+	 * @param event The event.
+	 */
+	@Mod.EventHandler
+	public void onPostInitialize (FMLPostInitializationEvent event) {
+		// TODO: Handle soft dependencies here
+
+		// call proxy
+		this.proxy.postInitialize ();
+	}
 }
