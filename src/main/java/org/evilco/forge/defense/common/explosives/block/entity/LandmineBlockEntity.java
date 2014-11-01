1e3ff7bd677c052f9a999d8008fc1e97ccbf7567
Johannes Donath <johannesd@evil-co.com>
2014-11-01 21:15:47 +0100

    Added explosion.

--- src/main/java/org/evilco/forge/defense/common/explosives/block/entity/LandmineBlockEntity.java
+++ src/main/java/org/evilco/forge/defense/common/explosives/block/entity/LandmineBlockEntity.java
@@ -14,7 +14,14 @@
  */
 package org.evilco.forge.defense.common.explosives.block.entity;
 
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
+import org.evilco.forge.defense.common.explosives.ExplosivesDamageSource;
+
+import java.util.List;
 
 /**
  * @author Johannes Donath <johannesd@evil-co.com>
@@ -26,4 +33,50 @@ public class LandmineBlockEntity extends TileEntity {
 	 * Defines the block entity name.
 	 */
 	public static final String NAME = "explosives_landmine";
+
+	/**
+	 * Returns the damage bounds.
+	 * @return The bounds.
+	 */
+	public AxisAlignedBB getDamageBounds () {
+		return AxisAlignedBB.getBoundingBox ((this.xCoord - 2), (this.yCoord - 2), (this.zCoord - 2), (this.xCoord + 2), (this.yCoord + 2), (this.zCoord + 2));
+	}
+
+	/**
+	 * Returns the detection bounds.
+	 * @return The bounds.
+	 */
+	public AxisAlignedBB getDetectionBounds () {
+		return AxisAlignedBB.getBoundingBox (this.xCoord, this.yCoord, this.zCoord, (this.xCoord + 1), (this.yCoord + 1), (this.zCoord + 1));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void updateEntity () {
+		super.updateEntity ();
+
+		// skip client update (if any)
+		if (this.worldObj.isRemote) return;
+
+		// get entities directly above
+		List<Entity> entities = this.worldObj.getEntitiesWithinAABBExcludingEntity (null, this.getDetectionBounds ());
+
+		// explode if needed
+		if (entities.size () > 0) {
+			// remove block
+			this.worldObj.setBlock (this.xCoord, this.yCoord, this.zCoord, Blocks.air);
+
+			// spawn explosion
+			this.worldObj.newExplosion (null, this.xCoord, this.yCoord, this.zCoord, 2.5f, true, true);
+
+			// damage all entities in radius
+			List<Entity> damageEntities = this.worldObj.getEntitiesWithinAABBExcludingEntity (null, this.getDamageBounds ());
+
+			for (Entity entity : damageEntities) {
+				entity.attackEntityFrom (ExplosivesDamageSource.LANDMINE, 24.0f);
+			}
+		}
+	}
 }
