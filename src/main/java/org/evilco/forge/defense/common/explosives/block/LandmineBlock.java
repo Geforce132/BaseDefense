68ed51d5279d5af4072bb41c7a02c07e907301c7
Johannes Donath <johannesd@evil-co.com>
2014-11-01 20:31:29 +0100

    Added block entity initialization, rendering fixes and bounds.

--- src/main/java/org/evilco/forge/defense/common/explosives/block/LandmineBlock.java
+++ src/main/java/org/evilco/forge/defense/common/explosives/block/LandmineBlock.java
@@ -18,9 +18,11 @@ import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import org.evilco.forge.defense.api.explosive.IExplosiveContainer;
 import org.evilco.forge.defense.common.DefenseCreativeTab;
+import org.evilco.forge.defense.common.explosives.block.entity.LandmineBlockEntity;
 
 /**
  * @author Johannes Donath <johannesd@evil-co.com>
@@ -40,6 +42,7 @@ public class LandmineBlock extends Block implements IExplosiveContainer, ITileEn
 		super (Material.tnt);
 
 		this.setCreativeTab (DefenseCreativeTab.EXPLOSIVES);
+		this.setBlockBounds (0.35f, 0.0f, 0.35f, 0.65f, 0.1f, 0.65f);
 	}
 
 	/**
@@ -47,7 +50,23 @@ public class LandmineBlock extends Block implements IExplosiveContainer, ITileEn
 	 */
 	@Override
 	public TileEntity createNewTileEntity (World p_149915_1_, int p_149915_2_) {
-		return null;
+		return (new LandmineBlockEntity ());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isOpaqueCube () {
+		return false;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean shouldSideBeRendered (IBlockAccess p_149646_1_, int p_149646_2_, int p_149646_3_, int p_149646_4_, int p_149646_5_) {
+		return false;
 	}
 
 	/**
