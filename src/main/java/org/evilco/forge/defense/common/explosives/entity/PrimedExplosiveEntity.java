f8d603adaf1ccbb190dd064e6e6c2d2f666d0a21
Johannes Donath <johannesd@evil-co.com>
2014-11-02 02:17:40 +0100

    Added gravity.

--- src/main/java/org/evilco/forge/defense/common/explosives/entity/PrimedExplosiveEntity.java
+++ src/main/java/org/evilco/forge/defense/common/explosives/entity/PrimedExplosiveEntity.java
@@ -79,6 +79,22 @@ public abstract class PrimedExplosiveEntity extends Entity {
 	public void onUpdate () {
 		super.onUpdate ();
 
+		// apply forces
+		this.prevPosX = this.posX;
+		this.prevPosY = this.posY;
+		this.prevPosZ = this.posZ;
+		this.motionY -= 0.03999999910593033D;
+		this.moveEntity (this.motionX, this.motionY, this.motionZ);
+		this.motionX *= 0.9800000190734863D;
+		this.motionY *= 0.9800000190734863D;
+		this.motionZ *= 0.9800000190734863D;
+
+		if (this.onGround) {
+			this.motionX *= 0.699999988079071D;
+			this.motionZ *= 0.699999988079071D;
+			this.motionY *= -0.5D;
+		}
+
 		// decrease fuse and go kaboom
 		if (this.fuse-- < 0) this.explode ();
 	}
